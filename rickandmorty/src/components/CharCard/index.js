1fd85cba488d0e0110872d02b14d19ed42231e95
Guilhermino Lucas <guilherminolucasc@gmail.com>
2021-09-19 13:41:44 -0300

    Improving my code

--- rickandmorty/src/components/CharCard/index.js
+++ rickandmorty/src/components/CharCard/index.js
@@ -1,18 +1,13 @@
 import "./style.css";
 const CharCard = ({ character }) => {
-  return character.status === "Alive" ? (
-    <div className="aliveCharacter character">
-      <p className="characterName"> Nome: {character.name} </p>
-      <img alt={character.name} src={character.image} />
-      <p>Origem: {character.origin.name}</p>
-      {character.status === "Alive" ? (
-        <p>Status: Vivo</p>
-      ) : (
-        <p>Status: Morto</p>
-      )}
-    </div>
-  ) : (
-    <div className="deadCharacter character">
+  return (
+    <div
+      className={
+        character.status === "Alive"
+          ? "aliveCharacter character"
+          : "deadCharacter character"
+      }
+    >
       <p className="characterName"> Nome: {character.name} </p>
       <img alt={character.name} src={character.image} />
       <p>Origem: {character.origin.name}</p>
@@ -23,5 +18,17 @@ const CharCard = ({ character }) => {
       )}
     </div>
   );
+  // ) : (
+  //   <div className="deadCharacter character">
+  //     <p className="characterName"> Nome: {character.name} </p>
+  //     <img alt={character.name} src={character.image} />
+  //     <p>Origem: {character.origin.name}</p>
+  //     {character.status === "Alive" ? (
+  //       <p>Status: Vivo</p>
+  //     ) : (
+  //       <p>Status: Morto</p>
+  //     )}
+  //   </div>
+  // );
 };
 export default CharCard;
