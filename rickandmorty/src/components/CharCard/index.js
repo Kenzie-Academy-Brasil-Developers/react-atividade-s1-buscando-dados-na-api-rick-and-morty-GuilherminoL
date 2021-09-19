dea55008824a376cb6db5f12353d95041d775d7b
Guilhermino Lucas <guilherminolucasc@gmail.com>
2021-09-19 13:43:28 -0300

    Deleting comments

--- rickandmorty/src/components/CharCard/index.js
+++ rickandmorty/src/components/CharCard/index.js
@@ -18,17 +18,5 @@ const CharCard = ({ character }) => {
       )}
     </div>
   );
-  // ) : (
-  //   <div className="deadCharacter character">
-  //     <p className="characterName"> Nome: {character.name} </p>
-  //     <img alt={character.name} src={character.image} />
-  //     <p>Origem: {character.origin.name}</p>
-  //     {character.status === "Alive" ? (
-  //       <p>Status: Vivo</p>
-  //     ) : (
-  //       <p>Status: Morto</p>
-  //     )}
-  //   </div>
-  // );
 };
 export default CharCard;
